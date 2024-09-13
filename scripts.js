6ec5c82659ed0b49d80bf15844abd11e1c361888
monir chelh <55603967+mconr@users.noreply.github.com>
2024-09-13 23:57:22 +0200

    update creating the second section

--- scripts.js
+++ scripts.js
@@ -5,49 +5,90 @@ $(document).ready(function() {
 });
 
 
-// light effect following the cursor 
-
-
-document.addEventListener("mousemove", (event) => {
-const button = document.querySelector("button");
-const rect = button.getBoundingClientRect();
-
-const buttonCenterX = rect.left + rect.width / 2;
-const buttonCenterY = rect.top + rect.height / 2;
-
-const distanceX = event.clientX - buttonCenterX;
-const distanceY = event.clientY - buttonCenterY;
-
-const strengths = {
-s1: 0.01,
-s2: 0.03,
-s3: 0.05,
-s4: 0.09,
-s5: 0.15,
-s6: -0.3,
-s7: 0
+// -------------------------------------------------- animation of the section 2
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
 };
 
-gsap.to(document.documentElement, {
-"--x1": `${-distanceX * strengths.s1}px`,
-"--y1": `${-distanceY * strengths.s1}px`,
-"--x2": `${-distanceX * strengths.s2}px`,
-"--y2": `${-distanceY * strengths.s2}px`,
-"--x3": `${-distanceX * strengths.s3}px`,
-"--y3": `${-distanceY * strengths.s3}px`,
-"--x4": `${-distanceX * strengths.s4}px`,
-"--y4": `${-distanceY * strengths.s4}px`,
-"--x5": `${-distanceX * strengths.s5}px`,
-"--y5": `${-distanceY * strengths.s5}px`,
-"--x6": `${-distanceX * strengths.s6}px`,
-"--y6": `${-distanceY * strengths.s6}px`,
-"--x7": `${-distanceX * strengths.s7}px`,
-"--y7": `${-distanceY * strengths.s7}px`,
-duration: 0.3,
-ease: "power2.out"
-});
-});
+const texts = [
+    "BE",
+    "A",
+    "SOFTWARE",
+    "ENGINEER",
+    "NOT",
+    "A",
+    "FRAMEWORKER",
+    ":)",
+    "by @MCONR"
+];
 
+const morphTime = 1;
+const cooldownTime = 0.25;
 
-// --------------------------------------------------
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
 
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
+        }
+
+        doMorph();
+    } else {
+        doCooldown();
+    }
+}
+
+animate();
